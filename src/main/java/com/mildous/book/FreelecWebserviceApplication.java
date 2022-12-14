bf8f4eefacc9d2276bd9e6177e8b7430be02d353
Mildous <9089_@naver.com>
2022-12-14 17:23:27 +0900

    Initial commit

---
+++ src/main/java/com/mildous/book/FreelecWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.mildous.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelecWebserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreelecWebserviceApplication.class, args);
+    }
+
+}
